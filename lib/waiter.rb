e1d5a7537b14fc3e356730e675e89a52ba5377e6
Romano Livi <romano_livi@outlook.com>
2020-12-21 21:10:00 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
-
+  attr_accessor :name, :yrs_experience
+  
+  @@all = []
+  
+  def initialize(name, age) 
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
